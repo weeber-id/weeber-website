841bef445ec234e866ac294c62e46a8493829f56
unknown <azharalifauzi@gmail.com>
2020-10-31 18:42:50 +0700

    edited redirecting page and integrate link tree to BE

--- client/src/pages/redirect/index.tsx
+++ client/src/pages/redirect/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
 import ReactPixel from 'react-facebook-pixel';
-import { LoadingMessage } from '../../components';
 import { useQuery } from '../../utils/use-query';
 
 const RedirectPage = () => {
   const url = useQuery().get('url');
+  const text = useQuery().get('text');
   const fb_event = useQuery().get('fb_event');
   const GA_event_action = useQuery().get('GA_event_action');
   const GA_event_category = useQuery().get('GA_event_category');
@@ -22,16 +22,40 @@ const RedirectPage = () => {
       });
     }
 
-    if (url) {
+    if (url && text) {
+      setTimeout(() => {
+        window.location.href = `${url}&text=${encodeURIComponent(text)}`;
+      }, 1500);
+    } else if (url && !text) {
       setTimeout(() => {
         window.location.href = url;
       }, 1500);
     }
-  }, [url, fb_event, GA_event_category, GA_event_action]);
+  }, [url, fb_event, GA_event_category, GA_event_action, text]);
 
   return (
-    <div>
-      <LoadingMessage message="Redirecting..." />
+    <div className="redirecting">
+      <div className="body-wrapper">
+        <div className="body">
+          <span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <div className="base">
+            <span></span>
+            <div className="face"></div>
+          </div>
+        </div>
+      </div>
+      <div className="longfazers">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <h1>Redirecting ...</h1>
     </div>
   );
 };
